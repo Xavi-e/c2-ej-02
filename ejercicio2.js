61b0b009e560070b893ba9119a38dc927441c12b
Xavier Escalante <xesgawork@gmail.com>
2021-05-26 23:37:00 +0200

    commit initial

---
+++ ejercicio2.js
@@ -0,0 +1,11 @@
+// Fase 1
+
+const frase = "megustaprogramar";
+
+console.log(frase[0].toUpperCase() + frase[1] + frase[2].toUpperCase() + frase.slice(3, 7) + frase[7].toUpperCase() + frase.slice(8, 16));
+
+// Fase 2
+
+const string = "NoMeGustaProgramar";
+
+console.log(string[0].tolowerCase() + string[1] + string[2].toLowerCase() + string[3] + string[5].tolowerCase + string.slice(5, 9) + string[9].toLowerCase() + string.slice(10, 18));
